04acb6e7984578d40253ae7a645e3fe439947345
CheBlankenship <cnakamura336@gmail.com>
2018-06-05 15:29:17 -0500

    add flowcharts

--- ch_02/lab_exercise/part_b/che_blankenship_Lab2.py
+++ ch_02/lab_exercise/part_b/che_blankenship_Lab2.py
@@ -1,24 +1,24 @@
 # Part B - a
 # Convert Miles to Kilometers
 milesToKm = float(input("William, please tell me how many miles you want to convert to kilometers >>> "))
-print("Converted %.2f mil to %.2f km." %(milesToKm, milesToKm * 1.6))
+print("William, %.2f mil is equal to %.2f km." %(milesToKm, milesToKm * 1.6))
 
 # Part B - b
 # Convert Faherenhit to Celsius
 fToC = float(input("William, please tell me how much faherenhit you want to convert to celsius >>> "))
-print("Converted %.2f F to %.2f C." %(fToC, ((fToC - 32) * 5 // 9)))
+print("William, %.2f F is equal to %.2f C." %(fToC, ((fToC - 32) * 5 // 9)))
 
 # Part B - c
 # Gallons to Liters
 galToL = float(input("William, please tell me how many gallons you want to convert to liters >>> "))
-print("Converted %.2f gal to %.2f L." %(galToL, galToL * 3.9)
+print("William, %.2f gal is equal to %.2f L." %(galToL, galToL * 3.9)
 
 # Part B - d
 # Pounds to Kilograms
 poundToKg = float(input("William, please tell me how many pounds you want to convert to kilograms >>> "))
-print("Converted %.2f pounds to %.2f kg." %(poundToKg, poundToKg * 0.45))
+print("William, %.2f pound is equal to %.2f kg." %(poundToKg, poundToKg * 0.45))
 
 # Part B - e
 # Inches to Centimeters
 inchesToCm = float(input("William, please tell me how many inches you want to convert to centimeters >>> "))
-print("Converted %.2f inch to %.2f cm." %(inchesToCm, inchesToCm * 2.54))
+print("William, %.2f inch is equal to %.2f cm." %(inchesToCm, inchesToCm * 2.54))
