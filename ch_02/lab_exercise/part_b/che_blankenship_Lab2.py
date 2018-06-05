851f45bf770e1aabce9d2a38951cc0e6e454786c
CheBlankenship <cnakamura336@gmail.com>
2018-06-05 15:13:25 -0500

    Change small misses

--- ch_02/lab_exercise/part_b/che_blankenship_Lab2.py
+++ ch_02/lab_exercise/part_b/che_blankenship_Lab2.py
@@ -1,28 +1,24 @@
-# Part A - a
+# Part B - a
 # Convert Miles to Kilometers
-milesToKm = float(input("William, please tell me how many miles you want to convert to kilometers >> "))
+milesToKm = float(input("William, please tell me how many miles you want to convert to kilometers >>> "))
 print("Converted %.2f mil to %.2f km." %(milesToKm, milesToKm * 1.6))
 
-
-# Part A - b
+# Part B - b
 # Convert Faherenhit to Celsius
-fToC = float(input("William, please tell me how much faherenhit you want to convert to celsius >> "))
-print("Converted %.2f F to %.2f C." %(fToC, fToC * 1.6))
+fToC = float(input("William, please tell me how much faherenhit you want to convert to celsius >>> "))
+print("Converted %.2f F to %.2f C." %(fToC, ((fToC - 32) * 5 // 9)))
 
-
-# Part A - c
+# Part B - c
 # Gallons to Liters
-galToL = float(input("William, please tell me how many gallons you want to convert to liters >> "))
-print("Converted %.2f gal to %.2f L." %(galToL, ((galToL * 5) * 5 // 9)))
+galToL = float(input("William, please tell me how many gallons you want to convert to liters >>> "))
+print("Converted %.2f gal to %.2f L." %(galToL, galToL * 3.9)
 
-
-# Part A - d
+# Part B - d
 # Pounds to Kilograms
-poundToKg = float(input("William, please tell me how many pounds you want to convert to kilograms >> "))
-print("Converted %.2f pounds to %.2f kg." %(poundToKg, 0.45 * poundToKg))
+poundToKg = float(input("William, please tell me how many pounds you want to convert to kilograms >>> "))
+print("Converted %.2f pounds to %.2f kg." %(poundToKg, poundToKg * 0.45))
 
-
-# Part A - e
+# Part B - e
 # Inches to Centimeters
-inchesToCm = float(input("William, please tell me how many inches you want to convert to centimeters >> "))
+inchesToCm = float(input("William, please tell me how many inches you want to convert to centimeters >>> "))
 print("Converted %.2f inch to %.2f cm." %(inchesToCm, inchesToCm * 2.54))
