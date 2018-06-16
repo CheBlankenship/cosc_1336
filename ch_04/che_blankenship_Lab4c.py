af74f5d47415a40d10ff997d5c7b663bfb12d257
CheBlankenship <cnakamura336@gmail.com>
2018-06-16 14:16:32 -0500

    Small fixes.

--- ch_04/che_blankenship_Lab4c.py
+++ ch_04/che_blankenship_Lab4c.py
@@ -7,43 +7,38 @@ grade_e = 50 # Over 50
 num_of_students = 0 # Counter
 total_score     = 0
 
+# Expect the input value which can be converted into type float.
+score = float(input("Enter student's score. Or enter -1 to stop. \n>>> "))
 
-score = input("Enter student's score. Or enter -1 to stop. \n>>> ")
-# Handle if the vlue is a valid input and enable to conver into type float.
-if ((score) and (float == type(float(score)))):
-    print("check >>> ")
-    if ((score) and (float == type(float(score)))):
-        score = float(score)
-        while score != -1:
-            num_of_students = num_of_students + 1
-            if score >= grade_a:
-                print('Congratulations! Your grade is A.')
+while (score != -1):
+    print("check")
+    score = float(score)
+    num_of_students = num_of_students + 1
+    if score >= grade_a:
+        print('Congratulations! Your grade is A.')
+        total_score = total_score + score
+    else:
+        if score >= grade_b:
+            print('Your grade is B. Good job!')
+            total_score = total_score + score
+        else:
+            if score >= grade_c:
+                print('Your grade is C. Soso.')
                 total_score = total_score + score
             else:
-                if score >= grade_b:
-                    print('Your grade is B. Good job!')
+                if score >= grade_d:
+                    print('Your grade is D...')
                     total_score = total_score + score
                 else:
-                    if score >= grade_c:
-                        print('Your grade is C. Soso.')
-                        total_score = total_score + score
-                    else:
-                        if score >= grade_d:
-                            print('Your grade is D...')
-                            total_score = total_score + score
-                        else:
-                            print('You made an F! Obviously you did not study.')
-                            total_score = total_score + score
-            score = float(input("Enter your score. \n>>> "))
-            # If input score is invalid and not able to convert into float, it will exit.
-            if not ((score) and (float == type(float(score)))):
-                print("Error: Invalid input. No charactors. Only numbers. ")
-        print("-----------------------------------------------")
-        print("Number of students   >> ", num_of_students, "students")
-        print("Total score          >> ", total_score)
-        print("Average score        >> ", total_score // num_of_students)
-        print("-----------------------------------------------")
-    else:
-        print("Error: Invalid input. No charactors. Only numbers. ")
-else:
-    print("Error: Invalid input. No charactors. Only numbers. ")
+                    print('You made an F! Obviously you did not study.')
+                    total_score = total_score + score
+
+    score = float(input("Enter student's score. Or enter -1 to stop. \n>>> "))
+
+# At the end of the program calculate a class average unless there were NO grades entered.
+if (total_score and num_of_students):
+    print("---------------------+-------------------------")
+    print("Number of students   |", num_of_students, "students")
+    print("Total score          |", total_score)
+    print("Average score        |", total_score // num_of_students)
+    print("---------------------+-------------------------")
