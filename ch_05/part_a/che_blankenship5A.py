69f63754fcc2f9ed714fd79d45c112643fea5cc4
CheBlankenship <cnakamura336@gmail.com>
2018-06-23 11:29:04 -0500

    fix the functions.

--- ch_05/part_a/che_blankenship5A.py
+++ ch_05/part_a/che_blankenship5A.py
@@ -1,4 +1,3 @@
-counter = 1 # Add +1 everytime the user enters a invalid input.
 nums    = "0123456789" # Expect input charactors to be in nums.
 
 
@@ -6,77 +5,59 @@ def main(): # Void
 
     ## 1, Mil to Km ##
     def milesToKm():
-        global counter
+        counter = 1
         while counter < 4:
-            milesToKm = input("William, please tell me how many miles you want to convert to kilometers >>> ")
+            m2k = input("William, please tell me how many miles you want to convert to kilometers >>> ")
             # Check if the input is valid or not.
-            if (milesToKm):
-                # Check if the input chars are only in nums.
-                # Success case: Best, Avg, Worst -> O(n).
-                # Negative case: Best->O(1), Avg->O(n/2), Worst->O(n)
+            if (m2k):
                 valid_num = True
-                for i in milesToKm:
+                for i in m2k:
                     if i not in nums:
                         valid_num = False
                 # If the input is a number, process it.
-                if (valid_num and not(float(milesToKm) < 0)):
-                    milesToKm = float(milesToKm)
-                    print(format(milesToKm, ".2f"), " mil is equal to ", format(milesToKm * 1.6, ".2f"), " km. \n\n")
-                    counter = 5
+                if (valid_num and not(float(m2k) < 0)):
+                    m2k = float(m2k)
+                    result = format(m2k, ".2f") + " mil is equal to " + format(m2k * 1.6, ".2f") + " km."
+                    return result
                 else:
                     print("Error: Invalid input. No negative numbers and charactors. Only positive numbers.\n")
                     counter = counter + 1
             else:
                 print("Error: Invalid input. No negative numbers and charactors. Only positive numbers.\n")
                 counter = counter + 1
-        # Reset.
-        if counter == 5:
-            counter = 1
 
-        return None
+
 
     ## 2, F to C ##
     def FahToCel():
-        global counter
+        counter = 1
         while counter < 4:
-            fToC = input("William, please tell me how much faherenhit you want to convert to celsius >>> ")
+            f2c = input("William, please tell me how much faherenhit you want to convert to celsius >>> ")
             # Check if the input is valid or not.
-            if (fToC):
-                # Check if the input chars are only in nums.
-                # Success case: Best, Avg, Worst -> O(n).
-                # Negative case: Best->O(1), Avg->O(n/2), Worst->O(n)
+            if (f2c):
                 valid_num = True
-                for i in fToC:
+                for i in f2c:
                     if i not in nums:
                         valid_num = False
                 # If the input is a number, process it.
-                if (valid_num and  not(float(fToC) < 1000)):
-                    fToC = float(fToC)
-                    print(format(fToC, ".2f") + " F is equal to " + format(((fToC - 32) * 5 // 9), ".2f") + " C. \n\n")
-                    counter = 5
+                if (valid_num and  not(float(f2c) < 1000)):
+                    f2c = float(f2c)
+                    return format(f2c, ".2f") + " F is equal to " + format(((f2c - 32) * 5 // 9), ".2f") + " C."
                 else:
                     print("Error: Invalid input. No negative numbers and charactors. Input needs to be larger than 1000.\n")
                     counter = counter + 1
             else:
                 print("Error: Invalid input. No negative numbers and charactors. Input needs to be larger than 1000.\n")
                 counter = counter + 1
-        # Reset.
-        if counter == 5:
-            counter = 1
-
-        return None
 
 
     ## 3, Gal to L ##
     def GalToLit():
-        global counter
+        counter = 1
         while counter < 4:
             galToL = input("William, please tell me how many gallons you want to convert to liters >>> ")
             # Check if the input is valid or not.
             if (galToL):
-                # Check if the input chars are only in nums.
-                # Success case: Best, Avg, Worst -> O(n).
-                # Negative case: Best->O(1), Avg->O(n/2), Worst->O(n)
                 valid_num = True
                 for i in galToL:
                     if i not in nums:
@@ -84,30 +65,22 @@ def main(): # Void
                 # If the input is a number, process it.
                 if (valid_num and not(float(galToL) < 0)):
                     galToL = float(galToL)
-                    print(format(galToL, ".2f"), " gal is equal to ", format(galToL * 3.9, ".2f"), " L. \n\n")
-                    counter = 5
+                    return format(galToL, ".2f") + " gal is equal to " + format(galToL * 3.9, ".2f") + " L."
                 else:
                     print("Error: Invalid input. No negative numbers and charactors. Only positive numbers.\n")
                     counter = counter + 1
             else:
                 print("Error: Invalid input. No negative numbers and charactors. Only positive numbers.\n")
                 counter = counter + 1
-        # Reset.
-        if counter == 5:
-            counter = 1
 
-        return None
 
     ## 4, Lb to Kg ##
     def PoundsToKg():
-        global counter
+        counter = 1
         while counter < 4:
             poundToKg = input("William, please tell me how many pounds you want to convert to kilograms >>> ")
             # Check if the input is valid or not.
             if (poundToKg):
-                # Check if the input chars are only in nums.
-                # Success case: Best, Avg, Worst -> O(n).
-                # Negative case: Best->O(1), Avg->O(n/2), Worst->O(n)
                 valid_num = True
                 for i in poundToKg:
                     if i not in nums:
@@ -115,30 +88,22 @@ def main(): # Void
                 # If the input is a number, process it.
                 if (valid_num and not(float(poundToKg) < 0)):
                     poundToKg = float(poundToKg)
-                    print(format(poundToKg, ".2f"), " pound is equal to ", format(poundToKg * 0.45, ".2f"), " kg. \n\n")
-                    counter = 5
+                    return format(poundToKg, ".2f") + " pound is equal to " + format(poundToKg * 0.45, ".2f") + " kg."
                 else:
                     print("Error: Invalid input. No negative numbers and charactors. Only positive numbers.\n")
                     counter = counter + 1
             else:
                 print("Error: Invalid input. No negative numbers and charactors. Only positive numbers.\n")
                 counter = counter + 1
-        # Reset.
-        if counter == 5:
-            counter = 1
 
-        return None
 
     ## 5, Inch to cm ##
     def InchesToCm():
-        global counter
+        counter = 1
         while counter < 4:
             inchesToCm = input("William, please tell me how many inches you want to convert to centimeters >>> ")
             # Check if the input is valid or not.
             if (inchesToCm):
-                # Check if the input chars are only in nums.
-                # Success case: Best, Avg, Worst -> O(n).
-                # Negative case: Best->O(1), Avg->O(n/2), Worst->O(n)
                 valid_num = True
                 for i in inchesToCm:
                     if i not in nums:
@@ -146,25 +111,25 @@ def main(): # Void
                 # If the input is a number, process it.
                 if (valid_num and not(float(inchesToCm) < 0)):
                     inchesToCm = float(inchesToCm)
-                    print(format(inchesToCm, ".2f") + " inch is equal to " + format(inchesToCm * 2.54, ".2f") + " cm. \n\n")
-                    counter = 5
+                    return format(inchesToCm, ".2f") + " inch is equal to " + format(inchesToCm * 2.54, ".2f") + " cm."
                 else:
                     print("Error: Invalid input. No negative numbers and charactors. Only positive numbers.\n")
                     counter = counter + 1
             else:
                 print("Error: Invalid input. No negative numbers and charactors. Only positive numbers.\n")
                 counter = counter + 1
-        # Reset.
-        if counter == 5:
-            counter = 1
-
-        return None
 
 
-    milesToKm()
-    FahToCel()
-    GalToLit()
-    PoundsToKg()
-    InchesToCm()
+
+    mTokm = milesToKm()
+    print(mTokm)
+    fToc = FahToCel()
+    print(fToc)
+    gTol = GalToLit()
+    print(gTol)
+    ptok = PoundsToKg()
+    print(ptok)
+    itoc = InchesToCm()
+    print(itoc)
 
 main()
